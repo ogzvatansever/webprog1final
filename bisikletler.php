9fe52dd1e80e666505334a415093f311a0ebf187
Oguz Vatansever <ogzvatansever@gmail.com>
2022-12-30 22:34:16 +0300

    added filtering to bike search

--- bisikletler.php
+++ bisikletler.php
@@ -37,26 +37,123 @@ error_reporting(E_ALL);
     </div>
   </div>
   <div class="album py-5 bg-light">
-    <div class="container overflow-auto">
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-2">
+      <div class="container filtreleme border">
+        <form action="#" method="POST">
+        <div class="row">
+          <h5 class="mt-4">Modeller</h5>
+          <?php
+
+          $modelsorgu = mysqli_query($conn,"select distinct bisiklet_model from bisikletler");
+          $modelsayi = mysqli_num_rows($modelsorgu);
+
+          $sqlQuery = "SELECT * FROM bisikletler WHERE id > 0";
+          if (isset($_POST['model'])) {
+            $modelFiltre = implode("','", $_POST['model']);
+            $sqlQuery .= " AND bisiklet_model IN ('".$modelFiltre."')";
+          }
+
+          while ($satir = mysqli_fetch_array($modelsorgu)) {
+            ?>
+            <div class="row">
+            <div class="col-1">
+            <input type="checkbox" id="model" name="model[]" value="<?php echo "$satir[0]"; ?>" <?php if (isset($_POST['model'])) if (in_array($satir[0],$_POST['model'])) echo'checked="checked"';  ?>>
+            </div>
+            <div class="col-10">
+            <label class="text-muted"><?php echo "$satir[0]"; ?></label>
+            </div>
+            </div>
+            <?php
+          }
+          
+          ?>
+        </div>
+        <hr/>
+        <div class="row">
+          <h5 class="mt-2">Markalar</h5>
+          <?php
+
+          $modelsorgu = mysqli_query($conn,"select distinct bisiklet_marka from bisikletler");
+          $modelsayi = mysqli_num_rows($modelsorgu);
+
+          if (isset($_POST['marka'])) {
+            $markaFiltre = implode("','", $_POST['marka']);
+            $sqlQuery .= " AND bisiklet_marka IN ('".$markaFiltre."')";
+          }
+
+          while ($satir = mysqli_fetch_array($modelsorgu)) {
+            ?>
+            <div class="row">
+            <div class="col-1">
+            <input type="checkbox" id="marka" name="marka[]" value="<?php echo "$satir[0]"; ?>" <?php if (isset($_POST['marka'])) if (in_array($satir[0],$_POST['marka'])) echo'checked="checked"';  ?>>
+            </div>
+            <div class="col-10">
+            <p class="text-muted"><?php echo "$satir[0]"; ?></p>
+            </div>
+            </div>
+            <?php
+          }
+
+          ?>
+
+        </div>
+        <hr/>
+        <div class="row">
+          <h5 class="mt-2">Seviyeler</h5>
+          <?php
+
+          $modelsorgu = mysqli_query($conn,"select distinct bisiklet_model_2 from bisikletler");
+          $modelsayi = mysqli_num_rows($modelsorgu);
+
+          if (isset($_POST['seviye'])) {
+            $seviyeFiltre = implode("','", $_POST['seviye']);
+            $sqlQuery .= " AND bisiklet_model_2 IN ('".$seviyeFiltre."')";
+          }
+
+          while ($satir = mysqli_fetch_array($modelsorgu)) {
+            ?>
+            <div class="row">
+            <div class="col-1">
+            <input type="checkbox" id="seviye" name="seviye[]" value="<?php echo "$satir[0]"; ?>" <?php if (isset($_POST['seviye'])) if (in_array($satir[0],$_POST['seviye'])) echo'checked="checked"';  ?>>
+            </div>
+            <div class="col-10">
+            <p class="text-muted"><?php if($satir[0] != "") echo "$satir[0]"; else echo "Base"; ?></p>
+            </div>
+            </div>
+            <?php
+          }
+
+          ?>
+
+        </div>
+        <div class="row mt-4">
+          <button type="submit" class="btn btn-block btn-dark" value="Submit">Filtrele</button>
+        </div>
+        </form>
+      </div>
+    </div>
+    <div class="col-9">
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
         <?php
-        $bisikletsorgu = mysqli_query($conn,"select * from bisikletler");
+        $bisikletsorgu = mysqli_query($conn,$sqlQuery);
         $bisikletsayi = mysqli_num_rows($bisikletsorgu);
 
         while ($satir = mysqli_fetch_array($bisikletsorgu)) {
           ?>
           <div class="col">
             <div class="card shadow">
-              <?php echo '<a href="bisiklet.php?id='.$satir[0].'">'; ?><img class="card-img-top p-2" src="img/<?php echo "$satir[0]";?>/1.webp" alt="<?php echo "$satir[1] $satir[2]" ?>" width="100%" height="auto"></a>
+              <?php echo '<a href="bisiklet.php?id='.$satir[0].'">'; ?><img class="card-img-top p-2" src="img/<?php echo "$satir[0]";?>/1.webp" alt="<?php if ($satir[2] == NULL) echo "$satir[1] $satir[3]"; else echo "$satir[2] $satir[3]"; ?>" width="100%" height="auto"></a>
               <div class="card-body">
-                <h5 class="card-title"><?php echo "$satir[1] $satir[2]"; ?></h5>
+                <h5 class="card-title"><?php if ($satir[2] == NULL) echo "$satir[1] $satir[3] $satir[4]"; else echo "$satir[2] $satir[3] $satir[4]"; ?></h5>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group mt-2">
                     <?php echo '<a href="bisiklet.php?id='.$satir[0].'">'; ?> <button type="button" class="btn btn-sm btn-outline-primary">İncele</button></a>
                   </div>
-                  <small class="text-muted"><?php $test3 = number_format($satir[3]); echo "$test3"; ?>$</small>
+                  <small class="text-muted"><?php $test3 = number_format($satir[5]); echo "$test3"; ?>$</small>
                 </div>
               </div>
             </div>
@@ -68,6 +165,8 @@ error_reporting(E_ALL);
       </div>
     </div>
   </div>
+      </div>
+      </div>
 
 </main>
 
@@ -83,7 +182,6 @@ error_reporting(E_ALL);
 
 
     <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
-
       
   </body>
 </html>
