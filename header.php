e8dbeed5eebdf24864830c5209302a4407c75cba
Oguz Vatansever <ogzvatansever@gmail.com>
2023-04-02 05:53:03 +0300

    shopping cart prototyping

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <header class="p-3 mb-5 border-bottom fixed-top bg-light">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+        <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 me-2 link-body-emphasis text-decoration-none">
           <img src="logo.svg" height="40px"></img>
         </a>
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -46,13 +46,44 @@
             <li><a class="dropdown-item" href="#">Çıkış Yap</a></li>
           </ul>
         </div>
-        <a href="#" type="button" class="link-dark position-relative ms-2 p-0">
+        
+        <a href="#" type="button" class="link-dark position-relative ms-2 p-0" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
         <i style="font-size: x-large;" class="bi bi-cart2"></i>
         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
         2
-        <span class="visually-hidden">unread messages</span>
         </span>
         </a>
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+          <div class="offcanvas-header">
+            <h3 class="offcanvas-title" id="offcanvasRightLabel">Sepetiniz</h3>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <hr>
+          <div class="offcanvas-body">
+            <div class="row">
+            <div class="card mb-3" style="max-width: 100%;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="img/1/1.webp" class="img-fluid rounded-start mt-2">
+                </div>
+                  <div class="col-md-8">
+                    <div class="card-body m-0 p-0">
+                      <!--<h5 class="card-title">S-Works Epic</h5>-->
+                      <p class="card-text m-0 mt-1 ms-2 p-0">S-Works Epic</p>
+                      <p class="card-text m-0 ms-2 p-0" style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;"><small class="text-body-secondary">M - SATIN CARBON / COLOR RUN BLUE MURANO PEARL / GLOSS CHROME FOIL LOGOS</small></p>
+                      <a href="#" class="link-danger"><p class="float-end mx-2"><i class="bi bi-x-square"></i></p></a>
+                      <a href="#" class="link-warning"><p class="float-end mx-1"><i class="bi bi-dash-square"></i></p></a>
+                      <p class="float-end">1</p>
+                      <a href="#" class="link-success"><p class="float-end mx-1"><i class="bi bi-plus-square"></i></p></a>
+                      <p class="card-text m-0 ms-2 p-0"><small class="text-body-secondary">12,000$</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </header>
