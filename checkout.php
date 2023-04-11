34ecb58eb9ea24b50fc7978767cff32bb1593d1d
Oguz Vatansever <ogzvatansever@gmail.com>
2023-04-11 23:56:15 +0300

    localized the page

--- checkout.php
+++ checkout.php
@@ -180,22 +180,22 @@ include("baglan.php");
         -->
       </div>
       <div class="col-md-7 col-lg-8">
-        <h4 class="mb-3">Billing address</h4>
+        <h4 class="mb-3">Fatura Adresi</h4>
         <form class="needs-validation" novalidate>
           <div class="row g-3">
             <div class="col-sm-6">
-              <label for="firstName" class="form-label">First name</label>
+              <label for="firstName" class="form-label">Adınız</label>
               <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
               <div class="invalid-feedback">
-                Valid first name is required.
+                Lütfen adınızı giriniz.
               </div>
             </div>
 
             <div class="col-sm-6">
-              <label for="lastName" class="form-label">Last name</label>
+              <label for="lastName" class="form-label">Soyadınız</label>
               <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
               <div class="invalid-feedback">
-                Valid last name is required.
+                Lütfen soyadınızı giriniz.
               </div>
             </div>
 
@@ -211,7 +211,7 @@ include("baglan.php");
             </div>
 
             <div class="col-12">
-              <label for="email" class="form-label">Email <span class="text-body-secondary">(Optional)</span></label>
+              <label for="email" class="form-label">E-posta <span class="text-body-secondary">(Optional)</span></label>
               <input type="email" class="form-control" id="email" placeholder="you@example.com">
               <div class="invalid-feedback">
                 Please enter a valid email address for shipping updates.
@@ -219,103 +219,76 @@ include("baglan.php");
             </div>
 
             <div class="col-12">
-              <label for="address" class="form-label">Address</label>
-              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+              <label for="address" class="form-label">Adress</label>
+              <input type="text" class="form-control" id="address" placeholder="1234 Hilal Sk" required>
               <div class="invalid-feedback">
-                Please enter your shipping address.
+                Lütfen adresinizi giriniz.
               </div>
             </div>
 
             <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-body-secondary">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+              <label for="address2" class="form-label">Adres 2 <span class="text-body-secondary">(İsteğe bağlı)</span></label>
+              <input type="text" class="form-control" id="address2" placeholder="Apartman ve kapı numarası">
             </div>
 
             <div class="col-md-5">
-              <label for="country" class="form-label">Country</label>
+              <label for="country" class="form-label">Ülke</label>
               <select class="form-select" id="country" required>
-                <option value="">Choose...</option>
-                <option>United States</option>
+                <option value="">Seçiniz...</option>
+                <option>Türkiye</option>
               </select>
               <div class="invalid-feedback">
-                Please select a valid country.
+                Lütfen doğru bir ülke seçiniz.
               </div>
             </div>
 
             <div class="col-md-4">
-              <label for="state" class="form-label">State</label>
+              <label for="state" class="form-label">Şehir</label>
               <select class="form-select" id="state" required>
-                <option value="">Choose...</option>
-                <option>California</option>
+                <option value="">Seçiniz...</option>
+                <option>Kocaeli</option>
               </select>
               <div class="invalid-feedback">
-                Please provide a valid state.
+                Lütfen doğru bir şehir seçiniz.
               </div>
             </div>
 
             <div class="col-md-3">
-              <label for="zip" class="form-label">Zip</label>
+              <label for="zip" class="form-label">Posta Kodu</label>
               <input type="text" class="form-control" id="zip" placeholder="" required>
               <div class="invalid-feedback">
-                Zip code required.
+                Posta kodu gereklidir.
               </div>
             </div>
           </div>
 
           <hr class="my-4">
 
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="same-address">
-            <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-          </div>
-
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="save-info">
-            <label class="form-check-label" for="save-info">Save this information for next time</label>
-          </div>
-
-          <hr class="my-4">
-
-          <h4 class="mb-3">Payment</h4>
-
-          <div class="my-3">
-            <div class="form-check">
-              <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
-              <label class="form-check-label" for="credit">Credit card</label>
-            </div>
-            <div class="form-check">
-              <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
-              <label class="form-check-label" for="debit">Debit card</label>
-            </div>
-            <div class="form-check">
-              <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
-              <label class="form-check-label" for="paypal">PayPal</label>
-            </div>
-          </div>
+          <h4 class="mb-3">Ödeme Yöntemi</h4>
 
           <div class="row gy-3">
             <div class="col-md-6">
-              <label for="cc-name" class="form-label">Name on card</label>
+              <label for="cc-name" class="form-label">Kart Sahibi</label>
               <input type="text" class="form-control" id="cc-name" placeholder="" required>
-              <small class="text-body-secondary">Full name as displayed on card</small>
+              <small class="text-body-secondary">Kart üzerinde yazdığı gibi isim soyisim</small>
               <div class="invalid-feedback">
-                Name on card is required
+                Kart üzerindeki isim gereklidir.
               </div>
             </div>
 
             <div class="col-md-6">
-              <label for="cc-number" class="form-label">Credit card number</label>
+              <label for="cc-number" class="form-label">Kredi Kartı Numarası</label>
               <input type="text" class="form-control" id="cc-number" placeholder="" required>
               <div class="invalid-feedback">
-                Credit card number is required
+                Kredi kartı numarası gereklidir.
               </div>
             </div>
 
             <div class="col-md-3">
-              <label for="cc-expiration" class="form-label">Expiration</label>
-              <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+              <label for="cc-expiration" class="form-label">Son Kullanma</label>
+              <input type="text" class="form-control" id="cc-expiration" placeholder="04/23" required>
               <div class="invalid-feedback">
-                Expiration date required
+                Son kullanma tarihi gereklidir.
               </div>
             </div>
 
@@ -323,14 +296,14 @@ include("baglan.php");
               <label for="cc-cvv" class="form-label">CVV</label>
               <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
               <div class="invalid-feedback">
-                Security code required
+                Güvenlik kodu gereklidir.
               </div>
             </div>
           </div>
 
           <hr class="my-4">
 
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Satın Al</button>
         </form>
       </div>
     </div>
