d8c0b9f0afd29941add5b9526014f04e983f3eeb
Oguz Vatansever <ogzvatansever@gmail.com>
2023-05-25 15:02:02 +0300

    sign-up prototyping

---
+++ sign-in/signup.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel="icon" href="../img/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <title>Kayıt Ol</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+    </style>
+</head>
+<body>
+<!--
+    Email
+    Password
+
+    Name
+    Surname
+    
+    Adress
+    Adress 2
+    Country
+    City
+    Zip
+ -->
+    <form>
+        <input type="form-control" class="form-control" id="yorumInputMail" name="mail" placeholder=" ">
+        <input type="form-control" class="form-control" id="yorumInputPassword" name="password" placeholder=" ">
+        <input type="form-control" class="form-control" id="yorumInputAdress" name="adress" placeholder=" ">
+        <input type="form-control" class="form-control" id="yorumInputAdress2" name="adress2" placeholder=" ">
+        <input type="form-control" class="form-control" id="yorumInputCountry" name="country" placeholder=" ">
+        <input type="form-control" class="form-control" id="yorumInputCity" name="city" placeholder=" ">
+        <input type="form-control" class="form-control" id="yorumInputZip" name="zip" placeholder=" ">
+    </form>
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+      
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script src="../js/sorgu.js"></script>
+</body>
+</html>
