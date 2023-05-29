68dfe17ecea7feabb30b4a9abf14f4b0bf927b74
Oguz Vatansever <ogzvatansever@gmail.com>
2023-05-29 13:19:41 +0300

    improved orders page

--- orders.php
+++ orders.php
@@ -38,12 +38,37 @@ if (!isset($_SESSION["logged-in"])) {
     }
     while ($satir = mysqli_fetch_array($siparissorgu)) {
         $siparisdetaysorgu = mysqli_query($conn,"SELECT * FROM sepet_detay WHERE sepet_id = ".$satir[1]);
+        ?>
+        <div class="row mt-3 border">
+        <div class="col-10">
+        <?php
+        $iccounter = 0;
         while ($detaysatir = mysqli_fetch_array($siparisdetaysorgu)) {
-            echo $detaysatir[2];
+            $testediyom = $conn -> query("SELECT * FROM bisikletler WHERE id = ".$detaysatir[2]) -> fetch_array();
+            if ($iccounter > 0) {
+                echo " &";
+            }
+            else $iccounter+=1;
+            ?>
+                <?php if ($testediyom[2] == NULL) echo "$testediyom[1]"; else echo "$testediyom[2]"; echo " $testediyom[3] $testediyom[4]"; echo " $detaysatir[3] x $detaysatir[4]";
         }
         ?>
-        <div class="row mt-3">
-            <div class="col">Column</div>
+        </div>
+        <div class="col-1">
+            <?php
+            $outprice = 0;
+            $siparisdetaysorgu = mysqli_query($conn,"SELECT * FROM sepet_detay WHERE sepet_id = ".$satir[1]);
+            while ($detaysatir = mysqli_fetch_array($siparisdetaysorgu)) {
+                $outprice += $conn -> query("SELECT bisiklet_fiyat FROM bisikletler WHERE id = ".$detaysatir[2]) -> fetch_column();
+            }
+            echo $outprice."$";
+            ?>
+        </div>
+        <div class="col-1">
+            <?php
+            if ($satir[2] == 0 ) echo "Onay bekliyor" ; else echo "Onaylandi";
+            ?>
+        </div>
         </div>
         <?php
     }
